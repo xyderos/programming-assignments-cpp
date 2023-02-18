83812d87c791ed81db1210814eba383d24ee3d57
xyderos <kostas@xyderos.com>
2023-02-18 12:39:55 +0100

    added four sun

---
+++ src/assignment.h
@@ -0,0 +1,7 @@
+// https://leetcode.com/problems/4sum/
+
+#include <set>
+#include <vector>
+
+auto four_sum(const std::vector<int> &array, int target)
+    -> std::set<std::vector<int>>;
--- tests/test_assignment.h
+++ tests/test_assignment.h
@@ -4,7 +4,7 @@ class test_assignment : public CPPUNIT_NS::TestCase {
 
     private:
 	CPPUNIT_TEST_SUITE(test_assignment);
-	CPPUNIT_TEST(first_test);
+	CPPUNIT_TEST(test_unique_quadruplets);
 
 	CPPUNIT_TEST_SUITE_END();
 
@@ -13,7 +13,7 @@ class test_assignment : public CPPUNIT_NS::TestCase {
 	void tearDown() override { }
 
     protected:
-	void first_test();
+	void test_unique_quadruplets();
 };
 
 CPPUNIT_TEST_SUITE_REGISTRATION(test_assignment);
